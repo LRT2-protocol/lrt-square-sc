84ec677f787a763d6ece55acdcdae6f391f76554
Shivam Agrawal <shivam@ether.fi>
2024-08-30 14:46:58 +0530

    forge fmt

--- src/PriceProvider.sol
+++ src/PriceProvider.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import "src/interfaces/IPriceProvider.sol";
 
-
 contract PriceProvider is IPriceProvider {
     uint256 public price;
 
---
+++ src/governance/Timelock.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract Timelock is TimelockController {
+    constructor(address[] memory proposers, address[] memory executors, address admin)
+        TimelockController(86400 * 2, proposers, executors, admin)
+    {}
+}
