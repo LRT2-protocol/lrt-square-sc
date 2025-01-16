cc8b4ef6efb648648b45e9554b93be9ff6574912
artemwoofsoftware <artem.m@woof.software>
2025-01-15 19:45:16 -0600

    fix: core impl address

--- safe-script/rename.ts
+++ safe-script/rename.ts
@@ -5,7 +5,7 @@ export async function rename() {
   const LRT_SQUARED_PROXY = "0x8F08B70456eb22f6109F57b8fafE862ED28E6040";
 
   const LRT_SQUARED_DUMMY_IMPL = "0x8E029cEDC7Daf4d9cFFe56AC6771dE266F3CCAdc";
-  const LRT_SQUARED_CORE_IMPL  = "0x3D987E04fC47ac625F720f169C658307fd9A16A2";
+  const LRT_SQUARED_CORE_IMPL  = "0x1cB489ef513E1Cc35C4657c91853A2E6fF1957dE";
 
   const adminAbi = [
     "function upgradeToAndCall(address newImplementation, bytes memory data) external"
