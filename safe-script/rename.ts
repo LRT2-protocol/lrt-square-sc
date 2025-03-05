f7ba011495c0f5a484951e84176b8a8efa9915ca
artemwoofsoftware <artem.m@woof.software>
2025-03-04 19:56:17 -0600

    Symbol and name renaming update (#10)
    * feat: init
    * fix: names
    * remove metadata from bytecode
    * fix: batch rename proccess, use storage pointers
    * feat: deployed impl
    * fix: core impl address
    * feat: update impl deployment address
    
    ---------
    
    Co-authored-by: syko <seongyun@postech.ac.kr>
    Co-authored-by: Dmitriy Babenko <159453675+dmitriy-woof-software@users.noreply.github.com>
    Co-authored-by: dmitriy-woof-software <dmitriy@woof.software>

---
+++ safe-script/rename.ts
@@ -0,0 +1,59 @@
+import { ethers } from "ethers";
+import { proposeBatch } from "./proposeBatch";
+
+export async function rename() {
+  const LRT_SQUARED_PROXY = "0x8F08B70456eb22f6109F57b8fafE862ED28E6040";
+
+  const LRT_SQUARED_DUMMY_IMPL = "0x8E029cEDC7Daf4d9cFFe56AC6771dE266F3CCAdc";
+  const LRT_SQUARED_CORE_IMPL  = "0x1cB489ef513E1Cc35C4657c91853A2E6fF1957dE";
+
+  const adminAbi = [
+    "function upgradeToAndCall(address newImplementation, bytes memory data) external"
+  ];
+  const adminIface = new ethers.utils.Interface(adminAbi);
+
+  const dummyAbi = [
+    "function setInfo(string memory _name, string memory _symbol) external"
+  ];
+  const dummyIface = new ethers.utils.Interface(dummyAbi);
+
+  const upgradeToDummyData = adminIface.encodeFunctionData(
+    "upgradeToAndCall",
+    [LRT_SQUARED_DUMMY_IMPL, "0x"]
+  );
+
+  const setInfoData = dummyIface.encodeFunctionData(
+    "setInfo",
+    ["King Protocol", "KING"]
+  );
+
+  const upgradeToCoreData = adminIface.encodeFunctionData(
+    "upgradeToAndCall",
+    [LRT_SQUARED_CORE_IMPL, "0x"]
+  );
+
+  const transactions = [
+    {
+      to: LRT_SQUARED_PROXY,
+      data: upgradeToDummyData,
+      value: "0"
+    },
+    {
+      to: LRT_SQUARED_PROXY,
+      data: setInfoData,
+      value: "0"
+    },
+    {
+      to: LRT_SQUARED_PROXY,
+      data: upgradeToCoreData,
+      value: "0"
+    }
+  ];
+
+  await proposeBatch(transactions);
+}
+
+rename().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
